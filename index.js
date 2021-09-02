3d9ca4b55c472c9d1a5b7b956ce6390d8420612b
beowulf <26135801+xbeowulfx@users.noreply.github.com>
2021-09-02 19:16:37 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
+const app = http.createServer((req, res) => {
   res.statusCode = 200;
   const msg = 'Hello Node!\n'
   res.end(msg);
 });
 
-server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}/`);
+app.listen('9000','0.0.0.0',()=>{
+      console.log("server is listening on 9000 port");
 });
