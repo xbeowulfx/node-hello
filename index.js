2d64235cd94ae5d13da8d38d9c3f9ab8b0d85b40
beowulf <26135801+xbeowulfx@users.noreply.github.com>
2021-09-02 21:02:55 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node! test adil1 \n'
+  const msg = 'Hello Node! test adil1 adil2test \n'
   res.end(msg);
 });
 
