e74542ca1bb4961bb5fea68ef3239ba1b20cdc90
beowulf <26135801+xbeowulfx@users.noreply.github.com>
2021-09-03 12:14:45 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node! test major genral \n'
+  const msg = 'Hello Node! test major genral kilvish \n'
   res.end(msg);
 });
 
